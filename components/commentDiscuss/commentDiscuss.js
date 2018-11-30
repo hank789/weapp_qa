a8e38b1d0e8784e9d0c6b53915703c85c2b3a43e
hank <hank.huiwang@gmail.com>
2018-11-30 12:40:27 +0800

    wx:key

--- components/commentDiscuss/commentDiscuss.js
+++ components/commentDiscuss/commentDiscuss.js
@@ -19,6 +19,27 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    upvote: function (e) {
+      if (!app.globalData.userInfo.mobile) {
+        this.triggerEvent('authPhone', {}, {});
+        return;
+      }
+      var that = this
+      console.log(this.data.productComments.feed.submission_id)
+      request.httpsPostRequest('/weapp/product/support/comment', {submission_id: this.data.productComments.feed.submission_id}, function (res_data) {
+        console.log(res_data);
+        if (res_data.code === 1000) {
+          that.setData({
+            'productComments.feed.support_number': res_data.data.type == 'upvote' ? that.data.productComments.feed.support_number + 1 : that.data.productComments.feed.support_number - 1
+          })
+        } else {
+          wx.showToast({
+            title: res_data.message,
+            icon: 'loading',
+            duration: 2000
+          });
+        }
+      })
+    }
   }
 })
