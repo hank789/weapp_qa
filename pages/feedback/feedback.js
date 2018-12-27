39db81cc0f2f015b154f9c58180edd0f8169ea19
陈灵灵 <798092775@qq.com>
2018-12-27 12:41:54 +0800

    信息反馈

---
+++ pages/feedback/feedback.js
@@ -0,0 +1,137 @@
+// pages/feedback/feedback.js
+var pictures = [];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    description: '',
+    pictures: [],
+    type: '',
+    feedBackInfo: [
+      {
+        id: 1,
+        text: '信息勘误'
+      },
+      {
+        id: 2,
+        text: '功能建议'
+      },
+      {
+        id: 3,
+        text: 'BUG反馈'
+      },
+      {
+        id: 4,
+        text: '不良举报'
+      },
+      {
+        id: 5,
+        text: '其他'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  feedBack: function (e) {
+    var that = this;
+    var text = e.currentTarget.dataset.name;
+    that.setData({
+      type: text
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.pictures // 需要预览的图片http链接列表
+    })
+  },
+  chooseImage: function () {
+    //上传图片相关
+    var that = this;
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        let tempFilePaths = res.tempFilePaths;
+
+        tempFilePaths.forEach(function (url, index) {
+          //   pictures.push(url);
+          //   that.setData({
+          //       pictures: pictures
+          //   });
+          let strRegex = "(.jpg|.png|.gif|.jpeg)$"; //用于验证图片扩展名的正则表达式
+          let re = new RegExp(strRegex);
+          if (re.test(url.toLowerCase())) {
+            let name = '' + index + '.' + url.split('.')[url.split('.').length - 1], localFile = url;
+            pictures.push(url);
+            that.setData({
+              pictures: pictures
+            });
+          } else {
+            throw "选择的不是图片";
+          }
+
+        });
+      }
+    });
+  }
+})
